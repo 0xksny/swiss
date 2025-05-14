9a9d880080926764946285c66cf84c774cb08c9e
Kevin Snyder <kevinsnyder@Kevins-MacBook-Pro.local>
2025-05-13 17:15:45 -0700

    Move convert logic to separate function
    Accepts the command as well as input and output arguments.
    
    Logic is unchanged other than producing the error on the appropriate command. This ensures that if the `convert` command fails, the error message will show the help for the `convert` command and not the root command.

--- src/main.rs
+++ src/main.rs
@@ -19,74 +19,87 @@ enum Commands {
     },
 }
 
+fn handle_convert(
+    command: &mut clap::Command,
+    input: Option<String>,
+    output: Option<String>,
+) -> Option<()> {
+    let input_path = input.unwrap_or_else(|| {
+        command
+            .error(ErrorKind::MissingRequiredArgument, "Input file is required")
+            .exit();
+    });
+
+    let output_path = output.unwrap_or_else(|| {
+        command
+            .error(
+                ErrorKind::MissingRequiredArgument,
+                "Output file is required",
+            )
+            .exit();
+    });
+
+    let input_type = util::get_type_from_path(&input_path).unwrap_or_else(|| {
+        command
+            .error(ErrorKind::InvalidValue, "Invalid input file type")
+            .exit();
+    });
+    let input_contents = std::fs::read_to_string(&input_path).unwrap_or_else(|_| {
+        command
+            .error(ErrorKind::Io, "Failed to read input file")
+            .exit();
+    });
+
+    let output_type = util::get_type_from_path(&output_path).unwrap_or_else(|| {
+        command
+            .error(ErrorKind::InvalidValue, "Invalid output file type")
+            .exit();
+    });
+
+    let output_contents = match (input_type, output_type) {
+        (util::Type::JSON, util::Type::YAML) => {
+            serde_json::from_str::<serde_json::Value>(&input_contents)
+                .map(|json| serde_yaml::to_string(&json).unwrap())
+                .unwrap_or_else(|_| {
+                    command
+                        .error(ErrorKind::InvalidValue, "Failed to convert JSON to YAML")
+                        .exit();
+                })
+        }
+        (util::Type::YAML, util::Type::JSON) => {
+            serde_yaml::from_str::<serde_yaml::Value>(&input_contents)
+                .map(|yaml| serde_json::to_string_pretty(&yaml).unwrap())
+                .unwrap_or_else(|_| {
+                    command
+                        .error(ErrorKind::InvalidValue, "Failed to convert YAML to JSON")
+                        .exit();
+                })
+        }
+        _ => {
+            command
+                .error(ErrorKind::InvalidValue, "Unsupported conversion type")
+                .exit();
+        }
+    };
+
+    std::fs::write(&output_path, output_contents).unwrap_or_else(|_| {
+        command
+            .error(ErrorKind::Io, "Failed to write output file")
+            .exit();
+    });
+
+    return Some(());
+}
+
 fn main() {
     let cli = Cli::parse();
 
+    let mut root_command = Cli::command();
+
     match cli.command {
         Some(Commands::Convert { input, output }) => {
-            let input_path = input.unwrap_or_else(|| {
-                Cli::command()
-                    .error(ErrorKind::MissingRequiredArgument, "Input file is required")
-                    .exit();
-            });
-
-            let output_path = output.unwrap_or_else(|| {
-                Cli::command()
-                    .error(
-                        ErrorKind::MissingRequiredArgument,
-                        "Output file is required",
-                    )
-                    .exit();
-            });
-
-            let input_type = util::get_type_from_path(&input_path).unwrap_or_else(|| {
-                Cli::command()
-                    .error(ErrorKind::InvalidValue, "Invalid input file type")
-                    .exit();
-            });
-            let input_contents = std::fs::read_to_string(&input_path).unwrap_or_else(|_| {
-                Cli::command()
-                    .error(ErrorKind::Io, "Failed to read input file")
-                    .exit();
-            });
-
-            let output_type = util::get_type_from_path(&output_path).unwrap_or_else(|| {
-                Cli::command()
-                    .error(ErrorKind::InvalidValue, "Invalid output file type")
-                    .exit();
-            });
-
-            let output_contents = match (input_type, output_type) {
-                (util::Type::JSON, util::Type::YAML) => {
-                    serde_json::from_str::<serde_json::Value>(&input_contents)
-                        .map(|json| serde_yaml::to_string(&json).unwrap())
-                        .unwrap_or_else(|_| {
-                            Cli::command()
-                                .error(ErrorKind::InvalidValue, "Failed to convert JSON to YAML")
-                                .exit();
-                        })
-                }
-                (util::Type::YAML, util::Type::JSON) => {
-                    serde_yaml::from_str::<serde_yaml::Value>(&input_contents)
-                        .map(|yaml| serde_json::to_string(&yaml).unwrap())
-                        .unwrap_or_else(|_| {
-                            Cli::command()
-                                .error(ErrorKind::InvalidValue, "Failed to convert YAML to JSON")
-                                .exit();
-                        })
-                }
-                _ => {
-                    Cli::command()
-                        .error(ErrorKind::InvalidValue, "Unsupported conversion type")
-                        .exit();
-                }
-            };
-
-            std::fs::write(&output_path, output_contents).unwrap_or_else(|_| {
-                Cli::command()
-                    .error(ErrorKind::Io, "Failed to write output file")
-                    .exit();
-            });
+            let command = root_command.find_subcommand_mut("convert").unwrap();
+            handle_convert(command, input, output);
         }
         None => {}
     }
